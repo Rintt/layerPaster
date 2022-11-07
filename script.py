984b84c267a4142ac1de17dcc723d43cc3835e93
rintt <ufukalgac@hotmail.com>
2022-11-07 15:49:15 +0100

    evolved the code into taking a variable number of objects to be pasted on top of the character, just add every type of item in a seperate folder starting with "item"

--- script.py
+++ script.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
 import sys
+from turtle import back
 from PIL import Image
 import os, glob
 
-backgrounds = './backgrounds'
-foregrounds = './foregrounds'
+backgrounds_file = './backgrounds'
+backgrounds_sans_character_file = './backgrounds_sans_character'
+foregrounds_file = './foregrounds'
+foregrounds2_file = './foregrounds2'
 save_path = './combinations'
-count = 0
-for background in os.listdir(backgrounds):
-  for foreground in os.listdir(foregrounds):
-    b = Image.open(backgrounds + "/" + background)
-    f = Image.open(foregrounds + "/" + foreground)
-    b.paste(f, (0, 0), f)
-    blended = Image.blend(b, f, alpha=0)
-    blended.save("./combinations/blended" + str(count) + ".png")
-    count = count + 1
-    print("created " + background + " + " + foreground)
+save_path2 = './combinations2'
+character = './character'
+
+def mixer(background, foreground, combinations):
+  print("Combining " + background[2:] + " and " + foreground[2:] + ", then pasting it into " + combinations[2:])
+  count = 0
+  for back in os.listdir(background):
+    for fore in os.listdir(foreground):
+      b = Image.open(background + "/" + back)
+      f = Image.open(foreground + "/" + fore)
+      b.paste(f, (0, 0), f)
+      blended = Image.blend(b, f, alpha=0)
+      blended.save(combinations + "/" + str(count) + ".png")
+      count = count + 1
+  print("Done!")
+
+mixer(backgrounds_sans_character_file, character, backgrounds_file)
+mixer(backgrounds_file, foregrounds_file, save_path)
+mixer(save_path, foregrounds2_file, save_path2)
 print("FINISHED!")
   
 
